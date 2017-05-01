f0dd9a8e840fd6686052d7a107f6b53368b16220
KuiHuaZi <liu758895539@qq.com>
2017-05-01 12:37:06 +0800

    完成Graph类和图常用函数的编写

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -2,7 +2,7 @@
 int degree(Graph G, int v)
 {
 	int degree = 0;
-	degree = G.adj(v).size();
+	degree = G.adj(v).degree;
 	return degree;
 }
 int maxDegree(Graph G)
@@ -24,12 +24,13 @@ int numberOfSelfloops(Graph G)
 	int count = 0;
 	for(int v = 0; v < G.V(); v++)
 	{
-		for (int w : G.adj(v))
+		const Vertex &vertex = G.adj(v);
+		const Edge *tmp = vertex.adj;
+		for (int i = 0; i < vertex.degree; i++)
 		{
-			if (w == v)
-			{
+			if (tmp->_v == v)
 				count++;
-			}
+				tmp = tmp->_next;
 		}
 	}
 	return count / 2;
@@ -40,11 +41,126 @@ ostream &operator <<(ostream&out, Graph G)
 	for (int v = 0; v < G.V(); v++)
 	{
 		out << v << ":";
-		for (int w : G.adj(v))
+		const Vertex &vertex = G.adj(v);
+		const Edge *tmp = vertex.adj;
+		for (int i = 0; i < vertex.degree; i++)
 		{
-			out << w << " ";
+			out << tmp->_v << " weight: " << tmp->_weight << "\n";
+			tmp = tmp->_next;
 		}
 		out << "\n";
 	}
 	return out;
+}
+Graph::Graph(int v)
+{
+	_v = v;
+	_e = 0;
+	_vtables = new Vertex[_v];
+}
+Graph::Graph(istream &in)
+{
+	int E;
+	cin >> _v >>E;
+	_vtables = new Vertex[_v];
+	for (int i = 0; i <= E; i++)
+	{
+		int v, w,weight;
+		cin >> v >> w>>weight;
+		addEdge(v, w,weight);
+	}
+
+}
+void Graph::addEdge(int v, int w,int weight)//到底有没有哨兵节点？
+{
+	Edge *tmp = _vtables[v].adj;
+	_vtables[v].adj = new Edge(w, weight, tmp);
+	_vtables[v].degree++;
+	tmp = _vtables[w].adj;
+	_vtables[w].adj = new Edge(v, weight, tmp);
+	_vtables[w].degree++;
+}
+const Vertex& Graph::adj(int v)
+{
+	if (v > _v)printf("input v bigger than _v\n");
+	return _vtables[v];
+}
+void Graph::removeVertex(int v)
+{
+	Edge *adj = _vtables[v].adj;
+	while (adj !=nullptr)
+	{
+		removeEdge(adj->_v, v);//移边操作自然会将adj中的边删除，所以不需要adj = ajd-》next；
+	}
+}
+void Graph::removeEdge(int v, int w)
+{
+	removeEdgeFromVtables(v, w);
+	removeEdgeFromVtables(w, v);
+	//Edge *tmp = _vtables[v].adj;
+	//Edge *preTmp = _vtables[v].adj;
+	//while (tmp != nullptr&&tmp->_v!=w)
+	//{
+	//	preTmp = tmp;
+	//	tmp = tmp->_next;
+	//}
+	//if (tmp != nullptr)
+	//{
+	//	preTmp->_next = tmp->_next;
+	//	delete tmp;
+	//	tmp = nullptr;
+	//}
+	//else
+	//{
+	//	printf("The Edge:%d -- %d do not exit\n", v, w);
+	//	return;
+	//}
+
+	//tmp = _vtables[w].adj;
+	//preTmp = _vtables[w].adj;
+	//while (tmp != nullptr&&tmp->_v != v)
+	//{
+	//	preTmp = tmp;
+	//	tmp = tmp->_next;
+	//}
+	//if (tmp != nullptr)
+	//{
+	//	preTmp->_next = tmp->_next;
+	//	delete tmp;
+	//	tmp = nullptr;
+	//}
+	//else
+	//{
+	//	printf("The Edge:%d -- %d do not exit\n", w, v);
+	//	return;
+	//}
+
+}
+void Graph::removeEdgeFromVtables(int vtables, int dest)
+{
+	Edge *tmp = _vtables[vtables].adj;
+	Edge *preTmp = _vtables[vtables].adj;
+	while (tmp != nullptr&&tmp->_v != dest)
+	{
+		preTmp = tmp;
+		tmp = tmp->_next;
+	}
+	if (tmp != nullptr)
+	{
+		preTmp->_next = tmp->_next;
+		delete tmp;
+		tmp = nullptr;
+	}
+	else
+	{
+		printf("The Edge:%d -- %d do not exit\n", vtables, dest);
+		return;
+	}
+}
+Graph::~Graph()
+{
+	for (int i = 0; i < _v; i++)
+	{
+		removeVertex(i);
+	}
 }
--- Graph/Graph.h
+++ Graph/Graph.h
@@ -3,18 +3,52 @@
 #include<string>
 #include<iostream>
 using namespace std;
+struct Edge
+{
+	int _weight;
+	int _v;//边指向的另外一个节点
+	Edge *_next;//下一条边
+	Edge(int v, int weight, Edge*next)
+	{
+		_v = v;
+		_weight = weight;
+		_next = next;
+	}
+	Edge()//默认构造尾链点
+	{
+		_v = -1;
+		_weight = -1;
+		_next = nullptr;
+	}
+};
+struct Vertex
+{
+	int degree;
+	Edge * adj;
+	Vertex()
+	{
+		degree = 0;
+		adj = nullptr;
+	}
+};
 class Graph
 {
 public:
 	Graph(int numOfV);//创建一个含有v个顶点但不含边的图
-	Graph(istream in);//从标准输入流读入一副图，先V，后E，然后顶点对
+	Graph(istream &in);//从标准输入流读入一副图，先V，后E，然后顶点对
 	int V() { return _v; }
 	int E() { return _e; }
-	void addEdge(int v, int w);//添加一条变v-w
-	vector<int>adj(int v);//顶点v的邻居节点
+	void addEdge(int v, int w,int weight);//添加一条变v-w
+	const Vertex &adj(int v);//顶点v的邻居节点
 	friend ostream &operator <<(ostream&out, Graph G);
+	~Graph();
+	void removeVertex(int v);
+	void removeEdge(int v, int w);
 private:
 	int _v;
 	int _e;
+	Vertex *_vtables;
+	void removeEdgeFromVtables(int v, int w);
+	
 };
 ostream &operator <<(ostream&out, Graph G);
